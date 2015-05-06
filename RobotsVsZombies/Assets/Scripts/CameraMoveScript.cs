129acb40eb841ffd26f428c999782ff9459efac9
Kendall Stilgenbauer <stilgeki@miamioh.edu>
2015-05-06 17:24:03 -0400

    Added Overhead Camera
    Overhead camera is functional.  It has revealed glaring scale problems,
    to be fixed in a later commit.

---
+++ RobotsVsZombies/Assets/Scripts/CameraMoveScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoveScript : MonoBehaviour {
+
+	public GameObject gameObject;
+	Vector3 position;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = gameObject.transform.position;
+
+		if (Input.GetKey (KeyCode.A)) {
+			//gameObject.transform.position.x++;
+			position.x--;
+		} else if (Input.GetKey (KeyCode.D)) {
+			//gameObject.transform.position.x--;
+			position.x++;
+		} else if (Input.GetKey (KeyCode.W)) {
+			//gameObject.transform.position.z++;
+			position.z++;
+		} else if (Input.GetKey (KeyCode.S)) {
+			//gameObject.transform.position.z--;
+			position.z--;
+		}
+
+		gameObject.transform.position = position;
+	}
+}
